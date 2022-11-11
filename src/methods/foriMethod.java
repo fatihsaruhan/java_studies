30969766c47f5ed6ba21ade0c1003a0eb20651e8
fatihsaruhan <fatih3023@gmail.com>
2022-11-11 23:12:06 +0300

    yerdegistirme yapildi

---
+++ src/methods/foriMethod.java
@@ -0,0 +1,12 @@
+package methods;
+//   Yerdeğiştirme metodu
+public class foriMethod {
+    public static void main(String[] args) {
+        int a=5;
+        int b=4;
+        int gecici =a;
+        a=b;
+        b=gecici;
+        System.out.println(a);
+    }
+}
